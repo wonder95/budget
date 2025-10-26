0d1ab723b32a517720d7418c0f70b8af68e55bd0
Steve Edwards <steve@smgaweb.com>
2025-10-25 22:13:23 -0700

    Add second Ziggy param in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,7 +23,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
+            .use(ZiggyVue, window.Ziggy)
             .mount(el);
     },
     progress: {
