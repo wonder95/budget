5b75992bfd9b3fa2364a517f9571d824b8701b36
Steve Edwards <steve@smgaweb.com>
2025-10-26 21:11:55 -0700

    Initial add of spending categories

--- app/Http/Controllers/SpendingCategoryController.php
+++ app/Http/Controllers/SpendingCategoryController.php
@@ -2,13 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateSpendingCategoryRequest;
+use App\Http\Resources\SpendingCategoryResource;
+use App\Models\SpendingCategory;
 use Inertia\Inertia;
 
 class SpendingCategoryController extends Controller
 {
     public function index()
     {
-        return Inertia::render('SpendingCategories/Index');
+        return Inertia::render('SpendingCategories/Index', [
+            'spendingCategories' => SpendingCategoryResource::collection(SpendingCategory::all())
+        ]);
+    }
+
+    public function store(CreateSpendingCategoryRequest $request)
+    {
+        $values = $request->validated();
+
+        $spendingCategory = SpendingCategory::create([
+            'name' => $values['name']
+        ]);
+
+        return redirect()->back()->with('success', 'Spending Category ' . $spendingCategory->name . ' created');
+    }
+
+    public function update(CreateSpendingCategoryRequest $request, SpendingCategory $category)
+    {
+        $values = $request->validated();
+
+        $category->update([
+            'name' => $values['name'],
+        ]);
+
+        return redirect()->back()->with('success', 'Spending Category ' . $category->name . ' updated');
     }
 }
--- routes/spending-categories.php
+++ routes/spending-categories.php
@@ -7,3 +7,10 @@ Route::get('/spending-categories', [SpendingCategoryController::class, 'index'])
     ->name('spending-categories.index')
     ->middleware(['auth']);
 
+Route::post('/spending-categories/create', [SpendingCategoryController::class, 'store'])
+    ->name('spending-categories.store')
+    ->middleware(['auth']);
+
+Route::put('/spending-categories/update/{category}', [SpendingCategoryController::class, 'update'])
+    ->name('spending-categories.update')
+    ->middleware(['auth']);
