4a359133f07d86fa70bcc5a7505e4331e498bff0
Steve Edwards <steve@smgaweb.com>
2025-11-29 00:01:19 -0800

    Initial add of RegisterIndex.vue

---
+++ resources/js/Pages/Accounts/RegisterIndex.vue
@@ -0,0 +1,43 @@
+<script setup>
+    import {Head, useForm} from '@inertiajs/vue3';
+    import { computed } from 'vue';
+    import PageHeader from '@/Components/PageHeader.vue';
+    import TextInput from '@/Components/TextInput.vue';
+
+const props = defineProps({
+    account: Object
+})
+
+const form = useForm({
+    vendor: '',
+    description: ''
+})
+
+const headerTitle = computed(() => {
+    return 'Register - ' + props.account.name;
+})
+
+</script>
+
+<template>
+    <Head title="Spending Categories "></Head>
+    <PageHeader :title="headerTitle" />
+
+    <h1 class="font-bold text-2xl">Register</h1>
+
+    <details class="max-w-2xl">
+        <summary class=" p-2 border border-black rounded-md">Add Entry</summary>
+        <div class="bg-blue-200 border border-slate-300">
+            <label for="vendor">Vendor</label>
+            <TextInput
+              type="text"
+              id="vendor"
+              v-model="form.vendor"
+              class=""
+              placeholder="Select vendor"
+            />
+            <label for="description">Description</label>
+            <input type="text" id="description">
+        </div>
+    </details>
+</template>
